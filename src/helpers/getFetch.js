f814d19a6fa7b56b755eee64df0ceb67204b7a94
Gaston Da Ruda <gastondaruda95@gmail.com>
2022-03-13 22:18:34 -0300

    problemas url image

--- src/helpers/getFetch.js
+++ src/helpers/getFetch.js
@@ -1,8 +1,9 @@
 const productos = [
     {id:1, nombre: 'Torta 1', description: 'Untipazo', stock: 5 , url: 'https://vadetartas.com/wp-content/uploads/2021/06/receta-torta-golosinera.png'},
     {id:2, nombre: 'Torta 2', description: 'Untipazo', stock: 7 , url: 'https://annaspasteleria.com/images/_imageBlock/DSC_3586web.jpg'},
-    {id:3, nombre: 'Torta 3', description: 'Untipazo', stock: 10 , url: 'https://images.aws.nestle.recipes/original/e89d19d331ac413811e3b1c11a893991_torta-princesa.jpg'}
-
+    {id:3, nombre: 'Torta 3', description: 'Untipazo', stock: 10 , url: 'https://images.aws.nestle.recipes/original/e89d19d331ac413811e3b1c11a893991_torta-princesa.jpg'},
+    {id:4, nombre: 'Torta 4', description: 'Untipazo', stock: 2 , url: '../assets/imagenes/IMG_1.jpg'},
+    {id:5, nombre: 'Torta 5', description: 'Untipazo', stock: 4 , url: 'img.jpg'}
 ]
 
 export const getFetch = new Promise((resolve, reject)=> {
